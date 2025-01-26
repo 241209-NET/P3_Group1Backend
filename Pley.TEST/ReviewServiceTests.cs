ba39abb1fc3d5f0dbdc218537645e87482b8b3a0
ELDHOSE\eldho <eldhosesalby@gmail.com>
2025-01-25 22:06:08 -0500

    reviewService 3 test added

--- Pley.TEST/ReviewServiceTests.cs
+++ Pley.TEST/ReviewServiceTests.cs
@@ -33,5 +33,44 @@ public class ReviewServiceTests
         Assert.Contains(result, r => r.Comment == "Not bad");
     }
 
-    
+    [Fact]
+    public void GetReviewByIdTest()
+    {
+        //Arrange
+        var mockRepo = new Mock<IReviewRepo>();
+        var review = new Review 
+        { 
+            Id = 1, Comment = "Great!", Rating = 5 
+        };
+
+        mockRepo.Setup(repo => repo.GetReviewById(1)).Returns(review);
+        
+        var reviewService = new ReviewService(mockRepo.Object, null);
+        
+        // Act
+        var result = reviewService.GetReviewById(1);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+        Assert.Equal("Great!", result.Comment);
+
+    }
+
+    [Fact]
+    public void GetReviewById_ReturnsNull_WhenReviewDoesNotExist()
+    {
+        // Arrange
+        var mockRepo = new Mock<IReviewRepo>();
+        mockRepo.Setup(repo => repo.GetReviewById(999)).Returns((Review?)null);
+        
+        var reviewService = new ReviewService(mockRepo.Object, null);
+        
+        // Act
+        var result = reviewService.GetReviewById(99);
+        
+        // Assert
+        Assert.Null(result);
+    }
+
 }
