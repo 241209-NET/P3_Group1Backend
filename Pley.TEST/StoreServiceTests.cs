76167ffc16212873470f32861b41c3ba17d10b45
ELDHOSE\eldho <eldhosesalby@gmail.com>
2025-01-26 04:44:10 -0500

    storeService 2 test added

--- Pley.TEST/StoreServiceTests.cs
+++ Pley.TEST/StoreServiceTests.cs
@@ -1,6 +1,53 @@
+using Moq;
+using Pley.API.DTO;
+using Pley.API.Model;
+using Pley.API.Repo;
+using Pley.API.Service;
+using Pley.API.Util;
+
 namespace Pley.TEST;
 
 public class StoreServiceTests
 {
-    
+    [Fact]
+    public void GetStoreById_ShouldReturn_WhenStoreExists()
+    {
+        // Arrange
+        var storeRepoMock = new Mock<IStoreRepo>();
+        var utility = new Utility(); 
+
+        var store = new Store 
+        { 
+            Id = 1, Username = "store1", Password = "password123"
+        };
+
+        storeRepoMock.Setup(repo => repo.GetStoreById(It.IsAny<int>())).Returns(store);
+
+        var storeService = new StoreService(storeRepoMock.Object, utility);
+
+        // Act
+        var result = storeService.GetStoreById(1);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("store1", result.Username);
+    }
+
+    [Fact]
+    public void GetStoreById_ShouldReturnNull_WhenStoreNotExist()
+    {
+        // Arrange
+        var storeRepoMock = new Mock<IStoreRepo>();
+        var utility = new Utility(); 
+
+        storeRepoMock.Setup(repo => repo.GetStoreById(It.IsAny<int>())).Returns((Store?)null);
+
+        var storeService = new StoreService(storeRepoMock.Object, utility);
+
+        // Act
+        var result = storeService.GetStoreById(99);
+
+        // Assert
+        Assert.Null(result);
+    }
 }
