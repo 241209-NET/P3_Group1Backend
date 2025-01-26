e9e9602248578b2516e35420c85034381eeaf767
Ludia Park <lypark1985@gmail.com>
2025-01-26 14:49:24 -0600

    migrations fixed

--- Pley.API/Migrations/20250126204834_first.Designer.cs
+++ Pley.API/Migrations/20250126204834_first.Designer.cs
@@ -12,7 +12,7 @@ using Pley.API.Data;
 namespace Pley.API.Migrations
 {
     [DbContext(typeof(PleyContext))]
-    [Migration("20250126201532_first")]
+    [Migration("20250126204834_first")]
     partial class first
     {
         /// <inheritdoc />
